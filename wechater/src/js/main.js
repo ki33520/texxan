6b610a5c705032a8f70d057092678f9d915d5b30
holyhu <ki33520@gmail.com>
2017-12-30 23:54:57 +0800

    add

--- wechater/src/js/main.js
+++ wechater/src/js/main.js
@@ -1,23 +1,22 @@
 /*!
  * vue-common.js
  */
+ $.fn.scrollSmooth = function(scrollHeight, duration) {
+	var $el = this;
+	var el  = $el[0];
+	var startPosition = el.scrollTop;
+	var delta = scrollHeight  - startPosition;
+	var startTime = Date.now();
+	function scroll() {
+		var fraction = Math.min(1, (Date.now() - startTime) / duration);
+		el.scrollTop = delta * fraction + startPosition;
+		if(fraction < 1) {
+			setTimeout(scroll, 10);
+		}
+	}
+	scroll();
+};
 var templates = {
-	bubble: '<div class="bubbles">\
-		<div class="bubble" v-for="(el,key) in pBubbles">\
-			<div class="bubble-inner flex" v-if="!el.right">\
-				<div class="avatar"><img :src="el.avatarpic" /></div>\
-				<div class="content">\
-					<div class="msg">{{el.msg}}</div>\
-				</div>\
-			</div>\
-			<div class="bubble-inner flex-h-right" v-if="el.right">\
-				<div class="content">\
-					<div class="msg">{{el.msg}}</div>\
-				</div>\
-				<div class="avatar"><img :src="el.avatarpic" /></div>\
-			</div>\
-		</div>\
-	</div>',
 	loadingPage: '<div id="loadingPage" v-on:animationend="animateEnd" class="loading-page animated" v-bind:class="{fadeOut: fadeout}" v-show="show">\
 		<div class="flex-hv-center h100">\
 			<div class="loading">\
@@ -27,20 +26,96 @@ var templates = {
 				</div>\
 			</div>\
 		</div>\
+	</div>',
+	message: '<div class="messages">\
+		<div class="wrap-messages" ref="wrapMessages" id="wrapMessages">\
+			<div class="wrap-messages-inner" ref="wrapMessagesInner" id="wrapMessagesInner">\
+				<div class="message animated fast slideInUp" v-for="(el,key) in pMessages">\
+					<div class="message-inner flex" v-if="!el.right">\
+						<div class="avatar"><img :src="el.avatarpic" /></div>\
+						<div class="content">\
+							<div class="bubble">\
+								<div class="bubble_cont">\
+									<div class="plain">\
+										<pre>{{el.msg}}</pre>\
+									</div>\
+								</div>\
+							</div>\
+						</div>\
+					</div>\
+					<div class="message-inner flex-h-right" v-if="el.right">\
+						<div class="content">\
+							<div class="bubble">\
+								<div class="bubble_cont">\
+									<div class="plain">\
+										<pre>{{el.msg}}</pre>\
+									</div>\
+								</div>\
+							</div>\
+						</div>\
+						<div class="avatar"><img :src="el.avatarpic" /></div>\
+					</div>\
+				</div>\
+			</div>\
+		</div>\
+	</div>',
+	messageEnter: '<div class="board-enter">\
+		<div class="board-top flex">\
+			<div class="input-enter icon-input flex-v-center">\
+				<p class="flex-h-right">{{nextEnterMsg}}</p>\
+				<span class="line-flash animated infinite slow flash"></span>\
+			</div>\
+		</div>\
+		<div class="board-key">\
+			<div class="btn icon-send flex-hv-center" v-bind:class="{active: sendBtnActive}" @keydown="send">发送</div>\
+		</div>\
+	</div>',
+	messageView: '<div class="message-view">\
+		<div class="messages">\
+			<div class="wrap-messages" ref="wrapMessages" id="wrapMessages">\
+				<div class="wrap-messages-inner" ref="wrapMessagesInner" id="wrapMessagesInner">\
+					<div class="message animated fast slideInUp" v-for="(el,key) in pMessages">\
+						<div class="message-inner flex" v-if="!el.right">\
+							<div class="avatar"><img :src="el.avatarpic" /></div>\
+							<div class="content">\
+								<div class="bubble">\
+									<div class="bubble_cont">\
+										<div class="plain">\
+											<pre>{{el.msg}}</pre>\
+										</div>\
+									</div>\
+								</div>\
+							</div>\
+						</div>\
+						<div class="message-inner flex-h-right" v-if="el.right">\
+							<div class="content">\
+								<div class="bubble">\
+									<div class="bubble_cont">\
+										<div class="plain">\
+											<pre>{{el.msg}}</pre>\
+										</div>\
+									</div>\
+								</div>\
+							</div>\
+							<div class="avatar"><img :src="el.avatarpic" /></div>\
+						</div>\
+					</div>\
+				</div>\
+			</div>\
+		</div>\
+		<div class="board-enter">\
+			<div class="board-top flex">\
+				<div class="input-enter icon-input flex-v-center">\
+					<p class="flex-h-right">{{nextEnterMsg}}</p>\
+					<span class="line-flash animated infinite slow flash"></span>\
+				</div>\
+			</div>\
+			<div class="board-key">\
+				<div class="btn icon-send flex-hv-center" v-bind:class="{active: sendBtnActive}">发送</div>\
+			</div>\
+		</div>\
 	</div>'
 }
-var bubble = Vue.extend({
-	template: templates.bubble,
-	data: function(){
-		return {
-			
-		}
-	},
-	props: ['pBubbles'],
-	methods: {
-		
-	}
-});
 
 var loadingPage = Vue.extend({
 	template: templates.loadingPage,
@@ -62,11 +137,138 @@ var loadingPage = Vue.extend({
 	}
 });
 
+var message = Vue.extend({
+	template: templates.message,
+	data: function(){
+		return {
+			
+		}
+	},
+	props: ['pMessages'],
+	watch: {
+		pMessages: {
+			handler: function(a,b){
+				var self = this;
+				this.$nextTick(function(){
+					var contentH = $(self.$refs.wrapMessagesInner).height();
+					var viewH = $(self.$refs.wrapMessages).height();
+					console.log(contentH,viewH)
+					$(self.$refs.wrapMessages).scrollSmooth(contentH - viewH + 16, 300)
+				});
+			},
+			deep: true
+		}
+	},
+	methods: {
+		
+	}
+});
+
+var messageEnter = Vue.extend({
+	template: templates.messageEnter,
+	data: function(){
+		return {
+			nextEnterMsg: '新年快乐啊新年快乐啊新年快乐啊新年快乐啊新年快乐啊!',
+			sendBtnActive: false
+		}
+	},
+	props: ['pMessages'],
+	methods: {
+		showOs: function(){
+			setTimeout(function(){
+
+			},1000)
+		},
+		send: function(e){
+			var self = this;
+			this.sendBtnActive = true;
+			setTimeout(function(){
+				self.pMessages.push({
+					right: true,
+					msg: self.nextEnterMsg,
+					avatarpic: 'src/images/logo.png',
+				})
+				self.nextEnterMsg = null;
+				self.sendBtnActive = false;
+				self.showOs()
+				// setTimeout(function(){
+				// 	self.$parent.show = 1;
+				// 	self.$parent.animated = 1;
+				// },2000)
+			},100)
+		}
+	},
+	mounted: function(){
+		var self = this;
+		setTimeout(function(){
+			self.send();
+		},1000)
+	}
+})
+
+var messageView = Vue.extend({
+	template: templates.messageView,
+	data: function(){
+		return {
+			nextEnterMsg: '新年快乐啊新年快乐啊新年快乐啊新年快乐啊新年快乐啊!',
+			sendBtnActive: false
+		}
+	},
+	props: ['pMessages'],
+	watch: {
+		pMessages: {
+			handler: function(a,b){
+				var self = this;
+				this.$nextTick(function(){
+					var contentH = $(self.$refs.wrapMessagesInner).height();
+					var viewH = $(self.$refs.wrapMessages).height();
+					console.log(contentH,viewH)
+					$(self.$refs.wrapMessages).scrollSmooth(contentH - viewH + 16, 300)
+				});
+			},
+			deep: true
+		}
+	},
+	methods: {
+		showOs: function(){
+			setTimeout(function(){
+
+			},1000)
+		},
+		send: function(e){
+			var self = this;
+			this.sendBtnActive = true;
+			setTimeout(function(){
+				self.pMessages.push({
+					right: true,
+					msg: self.nextEnterMsg,
+					avatarpic: 'src/images/logo.png',
+				})
+				self.nextEnterMsg = null;
+				self.sendBtnActive = false;
+				self.showOs()
+				// setTimeout(function(){
+				// 	self.$parent.show = 1;
+				// 	self.$parent.animated = 1;
+				// },2000)
+			},100)
+		}
+	},
+	mounted: function(){
+		var self = this;
+		setTimeout(function(){
+			self.send();
+		},1000)
+	}
+})
+
 var indexApp = new Vue({
 	el: '#app',
 	data : function(){
 		return {
-			bubbles: [
+			step: 0,
+			percent: 0,
+			messages1: [
 				{
 					msg: '爸爸妈妈新年快乐',
 					avatarpic: 'src/images/logo.png',
@@ -89,15 +291,35 @@ var indexApp = new Vue({
 					right: true,
 					msg: '新年快乐啊2',
 					avatarpic: 'src/images/logo.png',
+				}
+			],
+			messages2: [
+				{
+					msg: '爸爸妈妈新年快乐',
+					avatarpic: 'src/images/logo.png',
 				},
 				{
 					right: true,
-					msg: '新年快乐啊3',
+					msg: '新年快乐啊',
+					avatarpic: 'src/images/logo.png',
+				},
+				{
+					msg: '爸爸妈妈新年快乐1',
+					avatarpic: 'src/images/logo.png',
+				},
+				{
+					right: true,
+					msg: '新年快乐啊1',
+					avatarpic: 'src/images/logo.png',
+				},
+				{
+					right: true,
+					msg: '新年快乐啊2',
 					avatarpic: 'src/images/logo.png',
 				}
 			],
-			percent: 0,
-			step: 0,
+			animate: 0,
+			show: 0
 		}
 	},
 	watch: {
@@ -109,8 +331,10 @@ var indexApp = new Vue({
 		}
 	},
 	components: {
-		'bubble-item': bubble,
+		'message-view': messageView,
+		'message-item': message,
 		'loading-page': loadingPage,
+		'message-enter': messageEnter
 	},
 	methods: {
 		viewStart: function(){
@@ -127,6 +351,10 @@ var indexApp = new Vue({
 					self.step = 1;
 				}
 			//},1)
+		},
+		animateEnd: function(){
+			this.animate = null;
+			console.log(this)
 		}
 	},
 	computed: {
